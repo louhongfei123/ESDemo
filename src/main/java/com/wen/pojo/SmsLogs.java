7006620cd85a4daed5d340345ff04846198a384e
文葛格 <919575255@qq.com>
2020-08-25 17:19:04 +0800

    备注

--- src/main/java/com/wen/pojo/SmsLogs.java
+++ src/main/java/com/wen/pojo/SmsLogs.java
@@ -12,18 +12,18 @@ import java.util.Date;
 public class SmsLogs {
 
     private String id;
-    private Date createDate;
-    private Date sendDate;
-    private String longCode;
-    private String mobile;
-    private String corpName;
-    private String smsContent;
-    private Integer state;
-    private Integer operatorId;
-    private String province;
-    private String ipAddr;
-    private Integer replyTotal;
-    private Long fee;
+    private Date createDate;  //创建时间String
+    private Date sendDate;  //发送时间 date
+    private String longCode;  //发送长号码 如 16092389287811
+    private String mobile;  //如 13000000000
+    private String corpName; //发送公司名称，需要分词检索
+    private String smsContent;  //下发短信内容，需要分词检索
+    private Integer state;   //短信下发状态 0 成功 1 失败 integer
+    private Integer operatorId;  //运营商编号1移动2联通3电信 integer
+    private String province;  //省份
+    private String ipAddr;   //下发服务器IP地址
+    private Integer replyTotal;  //短信状态报告返回时长 integer
+    private Long fee;   // 	扣费 integer
 
 
 }
